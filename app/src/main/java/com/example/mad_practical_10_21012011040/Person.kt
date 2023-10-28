5319a53214442bec76fff95703ebb67a0c4bc82a
Harshal <harshalkotadiya20@gmail.com>
2023-10-28 10:41:37 +0530

    /

--- app/src/main/java/com/example/mad_practical_10_21012011040/Person.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011040/Person.kt
@@ -1,6 +1,8 @@
 package com.example.mad_practical_10_21012011040
 
+import org.json.JSONObject
 import java.io.Serializable
+import java.util.jar.Attributes.Name
 
 class Person ( var id:String,
                var name:String,
@@ -8,6 +10,19 @@ class Person ( var id:String,
                var phoneNo:String,
                var address:String,
                var latitude:Double,
-               var longitude:Double
-):Serializable{
+               var longitude:Double):Serializable{
+    constructor(jsonObject:JSONObject):this("","","","","",0.0,8.0) {
+        id = jsonObject.getString("id")
+        emailId = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
+
+
+
 }
--- app/src/main/java/com/example/mad_practical_10_21012011040/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011040/PersonAdapter.kt
@@ -2,15 +2,34 @@ package com.example.mad_practical_10_21012011040
 
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
 
-class PersonAdapter(val context: Context,val personArray:ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray)
+class PersonAdapter( context: Context,val personArray:ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray)
 {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view=LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+        val contact = getItem(position) // Access the Contact object from the adapter
+        view.findViewById<TextView>(R.id.person_name).text = contact?.name
+        view.findViewById<TextView>(R.id.personnumber).text = contact?.phoneNo
+        view.findViewById<TextView>(R.id.personemail).text = contact?.emailId
+        view.findViewById<TextView>(R.id.personaddress).text = contact?.address
+        val button1: Button = view.findViewById(R.id.locationBtn)
+
+        button1.setOnClickListener {
+            // Start the MapsActivity when button1 is clicked
+            val intent = Intent(context, MapsActivity::class.java)
+            context.startActivity(intent)
+        }
+
+
+
+
         return view
     }
 }
